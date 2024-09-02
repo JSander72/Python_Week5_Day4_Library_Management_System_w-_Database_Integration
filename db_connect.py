d2d0478328e825af36e912b6ea2a5d3d4a9bf57d
JSander72 <jamesearlsanders2@outlook.com>
2024-09-02 01:16:12 -0400

    forth commit

--- db_connect.py
+++ db_connect.py
@@ -1,38 +1,61 @@
+# import mysql.connector
+# from mysql.connector import Error
+
+# db_name = 'library_db'
+# host = 'localhost'
+# user = 'root'
+# password = '12@Wsxdr56'
+
+
+# try: 
+#     conn = mysql.connector.connect(
+#         database = db_name,
+#         host = host,
+#         user = user,
+#         password = password,
+        
+        
+#     )
+#     if conn.is_connected():
+#         print("Connections to MySQL databsse successful!")
+
+#     cursor = conn.cursor() #creating a cursor to act as a middle man between python and MySQL
+
+#     quary = "SELECT * FROM customer;"
+
+#     cursor.execute(quary)
+
+#     for row in cursor.fecthall():
+#         print(row)
+
+# except Error as e:
+#     print("Error while connecting to MySQL", e)
+
+# finally:
+#     if conn and conn.is_connected():
+#         cursor.close()
+#         conn.close()
+#         print("MySQL connection is closed")
+
 import mysql.connector
-from mysql.connector import Error
 
-db_name = 'library_db'
-host = 'localhost'
-user = 'root'
-password = '12@Wsxdr56'
-
-
-try: 
+try:
     conn = mysql.connector.connect(
-        database = db_name,
-        host = host,
-        user = user,
-        password = password,
-        
-        
+        host="localhost",
+        user="root",
+        password="YES", 
+        database="library_db"
     )
+
     if conn.is_connected():
-        print("Connections to MySQL databsse successful!")
+        print("Connected to MySQL database!")
+    else:
+        print("Failed to connect to MySQL database.")
 
-    cursor = conn.cursor() #creating a cursor to act as a middle man between python and MySQL
-
-    quary = "SELECT * FROM customer;"
-
-    cursor.execute(quary)
-
-    for row in cursor.fecthall():
-        print(row)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 finally:
-    if conn and conn.is_connected():
-        cursor.close()
+    if conn.is_connected():
         conn.close()
         print("MySQL connection is closed")
